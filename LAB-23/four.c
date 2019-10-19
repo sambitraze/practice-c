f044a2041201fc434e6574a4b7418814d4084141
SAMBIT KUMAR MAJHI <majhisambit2@gmail.com>
2019-10-19 22:31:06 +0530

    upload and update

---
+++ LAB-23/four.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct distance
+{
+    int km,m;
+};
+void main()
+{
+    struct distance d1,d2;
+    int res_km,res_m;
+    printf("\n\n\t\t Enter data for distance 1:");
+    printf("\n\nEnter KiloMeter:");
+    scanf("%d",&d1.km);
+    printf("\nEnter Meter:");
+    scanf("%d",&d1.m);
+    printf("\n\n\t\t Enter data for distance 2:");
+    printf("\n\nEnter KiloMeter:");
+    scanf("%d",&d2.km);
+    printf("\nEnter Meter:");
+    scanf("%d",&d2.m);
+
+    res_km=d1.km + d2.km + ((d1.m+d2.m)/1000);
+    res_m=((d1.m+d2.m)%1000);
+
+    printf("\n\n\t\t Resultant Distance:");
+    printf("\nResultant KiloMeter: %d",res_km);
+    printf("\nResultant Meter: %d",res_m);
+        
+}
